cb079a0141b3f0477185ca12e1ba28ea1242d13a
kabilan2370 <kabilan848964@gmail.com>
2026-01-12 14:58:16 +0530

    updated

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "app" {
   ami                    = "ami-0ecb62995f68bb549"
   instance_type          = var.instance_type
-  subnet_id              = aws_subnet.public[0].id
+  subnet_id              = aws_subnet.private[0].id
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
   key_name               = var.key_name
 
