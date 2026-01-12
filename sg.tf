6402d21596d3d1999bda6916d45270d029d313b7
kabilan2370 <kabilan848964@gmail.com>
2026-01-12 14:10:18 +0530

    updated

--- sg.tf
+++ sg.tf
@@ -26,18 +26,12 @@ resource "aws_security_group" "ec2_sg" {
     security_groups = [aws_security_group.alb_sg.id]
   }
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.1.0/32"]
-  }
 
   ingress {
-    from_port   = 1337
-    to_port     = 1337
+    from_port   = 5432
+    to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = ["10.0.2.0/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
